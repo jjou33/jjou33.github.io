027cfd4fe8e3394f6eaa571291f84bd17746f2f0
jjou33 <zanda33@naver.com>
2021-09-12 22:14:05 +0900

    210912 Blog Update

--- _pages/categories/category-vuestudy.md
+++ _pages/categories/category-vuestudy.md
@@ -1,11 +1,11 @@
 ---
 title: "Vue Study 기록"
 layout: archive
-permalink: categories/vuestudy
+permalink: categories/Vue
 author_profile: true
 sidebar_main: true
 ---
 
 
-{% assign posts = site.categories.vuestudy %}
+{% assign posts = site.categories.Vue %}
 {% for post in posts %} {% include archive-single2.html type=page.entries_layout %} {% endfor %}
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,8 +1,8 @@
 var store = [{
-        "title": "1. 삼각형 가능 조건",
-        "excerpt":" console.log   ","categories": ["Algorithm"],
+        "title": "github.io 블로그 시작하기",
+        "excerpt":" ","categories": ["JavaScript"],
         "tags": ["Programming"],
-        "url": "/algorithm/cat-test/",
+        "url": "/javascript/first-post-copy/",
         "teaser": null
       },{
         "title": "github.io 블로그 시작하기",
@@ -12,8 +12,8 @@ var store = [{
         "teaser": null
       },{
         "title": "github.io 블로그 시작하기",
-        "excerpt":"GitHub Blog 서비스인 github.io 블로그 시작하기로 했다. GitHub Blog 서비스의 이름은 Pages이다. Pages가 다른 블로그 플랫폼 보다 편한 것 같아서 마음에 든다. 다른 사람들도 같이 많이 사용했으면 좋겠다는 생각이 든다. YFM에서 정의한 제목을 이중 괄호 구문으로 본문에 추가할 수 있다. 이 글의 제목은 github.io 블로그 시작하기이고 마지막으로 수정된 시간은 2019-04-13...","categories": ["vuestudy"],
+        "excerpt":"GitHub Blog 서비스인 github.io 블로그 시작하기로 했다. GitHub Blog 서비스의 이름은 Pages이다. Pages가 다른 블로그 플랫폼 보다 편한 것 같아서 마음에 든다. 다른 사람들도 같이 많이 사용했으면 좋겠다는 생각이 든다. YFM에서 정의한 제목을 이중 괄호 구문으로 본문에 추가할 수 있다. 이 글의 제목은 github.io 블로그 시작하기이고 마지막으로 수정된 시간은 2019-04-13...","categories": ["Vue"],
         "tags": ["Blog"],
-        "url": "/vuestudy/cat-test2/",
+        "url": "/vue/cat-test2/",
         "teaser": null
       }]
