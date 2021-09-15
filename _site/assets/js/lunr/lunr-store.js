15656b398ad5fed1ec9d0b525408b83dd934177f
jjou33 <zanda33@naver.com>
2021-09-15 15:50:09 +0900

    Solution Update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "github.io 블로그 시작하기",
-        "excerpt":" ","categories": ["JavaScript"],
+        "excerpt":"1. 최소값 구하기   1. 최소값 구하기   1. 최소값 구하기   1. 최소값 구하기   1. 최소값 구하기   1. 최소값 구하기   1. 최소값 구하기   1. 최소값 구하기   ","categories": ["JavaScript"],
         "tags": ["Programming"],
         "url": "/javascript/first-post-copy/",
         "teaser": null
@@ -11,9 +11,15 @@ var store = [{
         "url": "/javascript/first-post/",
         "teaser": null
       },{
-        "title": "github.io 블로그 시작하기",
-        "excerpt":"GitHub Blog 서비스인 github.io 블로그 시작하기로 했다. GitHub Blog 서비스의 이름은 Pages이다. Pages가 다른 블로그 플랫폼 보다 편한 것 같아서 마음에 든다. 다른 사람들도 같이 많이 사용했으면 좋겠다는 생각이 든다. YFM에서 정의한 제목을 이중 괄호 구문으로 본문에 추가할 수 있다. 이 글의 제목은 github.io 블로그 시작하기이고 마지막으로 수정된 시간은 2019-04-13...","categories": ["Vue"],
-        "tags": ["Blog"],
-        "url": "/vue/cat-test2/",
+        "title": "앞 수들 보다 큰 수만 구하기",
+        "excerpt":":raising_hand: 알고리즘 공부 간 기록이 필요한 문제들에 대한 정리를 기록하는 포스트 입니다. 1. 문제 요건 입력값으로 N X N 행의 배열이 주어지고 해당 배열은 사람의 키를 의미한다. 맨앞에서 볼 때 보이는 사람일경우 즉, 자신의 앞 모든사람보다 키가 큰 사람을 구하라 2. 코드 기본 솔루션 function solution(arr) { let count =...","categories": ["Algorithm"],
+        "tags": ["Programming"],
+        "url": "/algorithm/2.%EC%95%9E-%EC%88%98%EB%93%A4-%EB%B3%B4%EB%8B%A4-%ED%81%B0-%EC%88%98%EB%A7%8C-%EA%B5%AC%ED%95%98%EA%B8%B0/",
+        "teaser": null
+      },{
+        "title": "최소값 구하기",
+        "excerpt":":raising_hand: 알고리즘 공부 간 기록이 필요한 문제들에 대한 정리를 기록하는 포스트 입니다. 1. 설명 Javascript 알고리즘을 푸는데 최소값을 구하는 방법은 여러가지 모듈을 사용할 수 도 있지만 모듈을 사용하지 않고 사용하는 방법이 있다. 아래와 같이 가장 작은 값을 선언 후 비교값을 돌면서 대체 하는 방식이다. 2. 코드 모듈 미사용 function solution(arr)...","categories": ["Algorithm"],
+        "tags": ["Programming"],
+        "url": "/algorithm/1.-%EC%B5%9C%EC%86%8C%EA%B0%92-%EA%B5%AC%ED%95%98%EA%B8%B0/",
         "teaser": null
       }]
