0d55c6bbb58d968074768c4589a58a8fa844a3c6
jjou33 <zanda33@naver.com>
2021-09-19 23:19:55 +0900

    posts Construct Change

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -14,13 +14,13 @@ var store = [{
         "title": "앞 수들 보다 큰 수만 구하기",
         "excerpt":":raising_hand: 알고리즘 공부 간 기록이 필요한 문제들에 대한 정리를 기록하는 포스트 입니다. 1. 문제 요건 입력값으로 N X N 행의 배열이 주어지고 해당 배열은 사람의 키를 의미한다. 맨앞에서 볼 때 보이는 사람일경우 즉, 자신의 앞 모든사람보다 키가 큰 사람을 구하라 2. 코드 기본 솔루션 function solution(arr) { let count =...","categories": ["Algorithm"],
         "tags": ["Programming","test","test1"],
-        "url": "/algorithm/2.%EC%95%9E-%EC%88%98%EB%93%A4-%EB%B3%B4%EB%8B%A4-%ED%81%B0-%EC%88%98%EB%A7%8C-%EA%B5%AC%ED%95%98%EA%B8%B0/",
+        "url": "/algorithm/%EC%95%9E-%EC%88%98%EB%93%A4-%EB%B3%B4%EB%8B%A4-%ED%81%B0-%EC%88%98%EB%A7%8C-%EA%B5%AC%ED%95%98%EA%B8%B0/",
         "teaser": null
       },{
         "title": "최소값 구하기",
         "excerpt":":raising_hand: 알고리즘 공부 간 기록이 필요한 문제들에 대한 정리를 기록하는 포스트 입니다. 1. 설명 Javascript 알고리즘을 푸는데 최소값을 구하는 방법은 여러가지 모듈을 사용할 수 도 있지만 모듈을 사용하지 않고 사용하는 방법이 있다. 아래와 같이 가장 작은 값을 선언 후 비교값을 돌면서 대체 하는 방식이다. 2. 코드 모듈 미사용 function solution(arr)...","categories": ["Algorithm"],
         "tags": ["Programming"],
-        "url": "/algorithm/1.-%EC%B5%9C%EC%86%8C%EA%B0%92-%EA%B5%AC%ED%95%98%EA%B8%B0/",
+        "url": "/algorithm/%EC%B5%9C%EC%86%8C%EA%B0%92-%EA%B5%AC%ED%95%98%EA%B8%B0/",
         "teaser": null
       },{
         "title": "격자판 합 구하기",
@@ -33,5 +33,11 @@ var store = [{
         "excerpt":":raising_hand: 알고리즘 공부 간 기록이 필요한 문제들에 대한 정리를 기록하는 포스트 입니다. 1. 문제 요건 N X N 길이의 격자판에서 상하좌우 값보다 큰 경우 봉우리로 인정 및 Count 주어진 N X N 배열 가장자리는 0 으로 가정하여 전체 봉우리의 갯수 를 구하라. 2. 코드 먼저 문제를 보자마자 기억나는 아이디어는 합을...","categories": ["Algorithm"],
         "tags": ["Programming"],
         "url": "/algorithm/%EB%B4%89%EC%9A%B0%EB%A6%AC-%EA%B0%AF%EC%88%98-%EA%B5%AC%ED%95%98%EA%B8%B0/",
+        "teaser": null
+      },{
+        "title": "회문 문자열",
+        "excerpt":":raising_hand: 알고리즘 공부 간 기록이 필요한 문제들에 대한 정리를 기록하는 포스트 입니다. 1. 문제 요건 특정 문자열(String) 이 주어지고 해당 문자열이 회문 문자열에 해당하는지 여부를 판단하라. 2. 코드 기본 솔루션 // 직접비교 function solution(str) { let answerFlag = \"YES\" let length = Math.floor(str.length / 2) // 어차피 짝수,홀수 개일경우에도 중간은...","categories": ["Algorithm"],
+        "tags": ["Programming"],
+        "url": "/algorithm/%ED%9A%8C%EB%AC%B8-%EB%AC%B8%EC%9E%90%EC%97%B4/",
         "teaser": null
       }]
